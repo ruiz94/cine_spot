8636be91045752e8d496fd298f40095b61a1ed70
ruiz94 <armando_ruiizz@hotmail.com>
2025-08-02 09:48:37 -0600

    CS-14 add underscore to req param to avoid eslint highlighted issue

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const PORT = process.env['PORT'] || 3000;
 
-app.get('/health', (req, res) => {
+app.get('/health', (_req, res) => {
   res.status(200).json({ status: 'ok'})
 });
 
