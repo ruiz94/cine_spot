e2046925146ea668245f0db1b538252b2cf3ab80
ruiz94 <armando_ruiizz@hotmail.com>
2025-11-06 13:59:58 -0600

    set a spetial pino config for production

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,13 +1,26 @@
 import pino from 'pino';
+import { isProduction } from '@/config';
 
-const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
-      ignore: 'pid,hostname',
-    },
-  },
-});
+const logger = pino(
+  isProduction
+    ? {
+        level: 'info',
+        formatters: {
+          level: (label) => {
+            return { level: label.toUpperCase() };
+          },
+        },
+        timestamp: pino.stdTimeFunctions.isoTime,
+      }
+    : {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
+            ignore: 'pid,hostname',
+          },
+        },
+      },
+);
 
 export default logger;
